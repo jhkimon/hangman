429b4ce389a16c945c39774c732bd77c5ec40c28
jhkimon <sky06098i1@gmail.com>
2024-06-25 16:23:29 +0900

    conflict done: guessedLetter hidden

--- hangmanapp/src/components/BlankWord.jsx
+++ hangmanapp/src/components/BlankWord.jsx
@@ -2,24 +2,32 @@ import { useEffect, useState } from 'react';
 
 function BlankWord(props) {
     const strArr = [...props.letter];
+    const correctWord = props.guessedLetters;
+
+    useEffect(() => {
+        const correctWord = props.guessedLetters;
+    }, [props.guessedLetters]);
+
     return (
         <>
             <div className="flex justify-center m-3">
-                {strArr.map((charElem, index) => {
-                    return (
-                        <span>
-                            {charElem == ' ' ? (
-                                <span className="w-8 p-2 m-2">
-                                    <span className=""></span>
+                {strArr.map((charElem, index) => (
+                    <span key={index}>
+                        {charElem === ' ' ? (
+                            <span className="w-8 p-2 m-2">
+                                <span className=""></span>
+                            </span>
+                        ) : (
+                            <span className="w-8 p-2 m-2 border-b-4 border-stone-500">
+                                <span className="font-extrabold text-2xl text-stone-900">
+                                    {props.guessedLetters != null && props.guessedLetters.includes(charElem)
+                                        ? charElem
+                                        : ''}
                                 </span>
-                            ) : (
-                                <span className="w-8 p-2 m-2 border-b-4 border-stone-500">
-                                    <span className="font-extrabold text-2xl text-stone-900">{charElem}</span>
-                                </span>
-                            )}
-                        </span>
-                    );
-                })}
+                            </span>
+                        )}
+                    </span>
+                ))}
             </div>
         </>
     );
