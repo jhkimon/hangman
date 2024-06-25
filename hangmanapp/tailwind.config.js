1bfa6d58723bf348b4a7238fe77e7c31d63331ef
jhkimon <sky06098i1@gmail.com>
2024-06-25 09:02:02 +0900

    first commit

---
+++ hangmanapp/tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                gray: colors.coolGray,
+                blue: colors.lightBlue,
+                red: colors.rose,
+                pink: colors.fuchsia,
+            },
+            fontFamily: {
+                sans: ['Graphik', 'sans-serif'],
+                serif: ['Merriweather', 'serif'],
+            },
+        },
+    },
+    plugins: [],
+};
