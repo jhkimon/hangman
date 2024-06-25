821af0ca9a41fc38534fa548e197939c55fe88b7
yjbae3974 <brianbae6@gmail.com>
2024-06-25 13:50:41 +0900

    merged

--- hangmanapp/src/pages/main.jsx
+++ hangmanapp/src/pages/main.jsx
@@ -28,8 +28,8 @@ function Main() {
             <ProgressBar progress={10} />
             <div className="container flex mt-24">
                 <div className="w-1/2 flex items-center flex-col">
-                    <ThemeCard theme={randomTheme} />
-                    <BlankWord letter={randomLetter} />
+                    <ThemeCard theme={theme} />
+                    <BlankWord letter={letter} />
                     <LetterWrapper>
                         {alphabet.map((letter, index) => {
                             return letter ? <LetterCard key={index} letter={letter} /> : null;
