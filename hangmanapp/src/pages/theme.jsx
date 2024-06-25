419c0210c2b88ada13327b4c29ca5c3b8e638cd2
yjbae3974 <brianbae6@gmail.com>
2024-06-25 18:14:37 +0900

    .

--- hangmanapp/src/pages/theme.jsx
+++ hangmanapp/src/pages/theme.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Button from '../components/Button'; // 경로 확인
-import setTheme from '../App';
-const themes = ['Food', 'Country', 'Suneung', 'Idol', 'NEXT', 'Random'];
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button"; // 경로 확인
+import setTheme from "../App";
+const themes = ["Food", "Country", "Suneung", "Idol", "NEXT", "Random"];
 
 function Theme() {
     const navigate = useNavigate();
 
     const handleButtonClick = (theme) => {
-        console.log('얍');
+        console.log("얍");
         navigate(`/main/${theme}`);
     };
 
     return (
         <div>
             {themes.map((theme) => (
-                <Button key={theme} letter={theme} onClick={() => handleButtonClick(theme)} />
+                <Button
+                    key={theme}
+                    letter={theme}
+                    onClick={() => handleButtonClick(theme)}
+                />
             ))}
         </div>
     );
