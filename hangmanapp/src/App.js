8530aed7c7244fdfcf49d6abb5629f6c92396f56
yjbae3974 <brianbae6@gmail.com>
2024-06-25 13:44:39 +0900

    added setState on app.js

--- hangmanapp/src/App.js
+++ hangmanapp/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import {useState, useEffect} from 'react';
 import Landing from './pages/landing';
 import Theme from './pages/theme';
 import Main from './pages/main';
 import Result from './pages/result';
 
 function App() {
+    const [score, setScore] = useState(0);
+    const [theme, setTheme] = useState('countries');
     return (
         <>
             <BrowserRouter>
