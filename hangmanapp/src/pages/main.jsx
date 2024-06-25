674e7dc0b5b5f2742192f0f70331745ac7dd4ea1
yjbae3974 <brianbae6@gmail.com>
2024-06-25 09:50:49 +0900

    added components

--- hangmanapp/src/pages/main.jsx
+++ hangmanapp/src/pages/main.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useEffect, useState } from 'react';
-import {Button} from '../components/Button';
-
+import React from "react";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { Button } from "../components/Button";
+import { BlankWord } from "../components/BlankWord";
+import { LetterCard } from "../components/LetterCard";
+import { ThemeCard } from "../components/ThemeCard";
+import { ScoreBoard } from "../components/ScoreBoard";
 const ModifiedButton = styled(Button)`
     margin: 10px;
-    
-`
+`;
 
 function Main() {
-  return (
-    <div>
-      
-      <ModifiedButton />
-    </div>
-  );
+    return (
+        <div>
+            <ThemeCard />
+            <BlankWord />
+            <LetterCard />
+            <ScoreBoard />
+            <ModifiedButton />
+        </div>
+    );
 }
 
 export default Main;
