19b89875286436fd9afb25f80fa0136f3a1ccef7
yjbae3974 <brianbae6@gmail.com>
2024-06-25 15:26:28 +0900

    completed letterClick

--- hangmanapp/src/components/LetterCard.jsx
+++ hangmanapp/src/components/LetterCard.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
-
 
 const LetterSpan = styled.div`
     display: inline-block;
@@ -14,18 +12,24 @@ const LetterSpan = styled.div`
     &:active {
         background-color: lightgray;
     }
+    background-color: ${({ isRight, isClicked }) => 
+        !isClicked ? 'white' :
+        isRight ? 'lightgreen' : 'rgb(248 113 113)'};
+    pointer-events: ${({ isClicked }) => isClicked ? 'none' : 'auto'};
+    opacity: ${({ isClicked }) => isClicked ? '0.2' : '1'};
+`;
 
-
-`
-
-function LetterCard({ letter, onClick }) {
-  return (
-    <>
-        <LetterSpan className='text-3xl hover:-translate-y-1 hover:scale-110 ease-in-out transform duration-500' onClick={onClick}>
+function LetterCard({ letter, onClick, isRight, isClicked }) {
+    return (
+        <LetterSpan 
+            className='text-3xl hover:-translate-y-1 hover:scale-110 ease-in-out transform duration-500' 
+            onClick={onClick}
+            isRight={isRight}
+            isClicked={isClicked}
+        >
             {letter}
         </LetterSpan>
-    </>
-  );
+    );
 }
 
 export default LetterCard;
--- hangmanapp/src/pages/main.jsx
+++ hangmanapp/src/pages/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useEffect, useState } from 'react';
-import Button from '../components/Button';
-import BlankWord from '../components/BlankWord';
-import LetterCard from '../components/LetterCard';
-import ThemeCard from '../components/ThemeCard';
-import ScoreBoard from '../components/ScoreBoard';
-import ProgressBar from '../components/ProgressBar';
-import CategorizedWords from '../components/CategorizedWords';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import Button from "../components/Button";
+import BlankWord from "../components/BlankWord";
+import LetterCard from "../components/LetterCard";
+import ThemeCard from "../components/ThemeCard";
+import ScoreBoard from "../components/ScoreBoard";
+import ProgressBar from "../components/ProgressBar";
+import CategorizedWords from "../components/CategorizedWords";
 
 const LetterWrapper = styled.div`
     display: flex;
@@ -19,20 +18,81 @@ const LetterWrapper = styled.div`
 
 function Main() {
     const { theme } = useParams();
-    console.log(theme);
-    const letter = CategorizedWords[theme][Math.floor(Math.random() * CategorizedWords[theme].length)];
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const [alphabet, setAlphabet] = useState([
+        { letter: "A", isRight: false, isClicked: false },
+        { letter: "B", isRight: false, isClicked: false },
+        { letter: "C", isRight: false, isClicked: false },
+        { letter: "D", isRight: false, isClicked: false },
+        { letter: "E", isRight: false, isClicked: false },
+        { letter: "F", isRight: false, isClicked: false },
+        { letter: "G", isRight: false, isClicked: false },
+        { letter: "H", isRight: false, isClicked: false },
+        { letter: "I", isRight: false, isClicked: false },
+        { letter: "J", isRight: false, isClicked: false },
+        { letter: "K", isRight: false, isClicked: false },
+        { letter: "L", isRight: false, isClicked: false },
+        { letter: "M", isRight: false, isClicked: false },
+        { letter: "N", isRight: false, isClicked: false },
+        { letter: "O", isRight: false, isClicked: false },
+        { letter: "P", isRight: false, isClicked: false },
+        { letter: "Q", isRight: false, isClicked: false },
+        { letter: "R", isRight: false, isClicked: false },
+        { letter: "S", isRight: false, isClicked: false },
+        { letter: "T", isRight: false, isClicked: false },
+        { letter: "U", isRight: false, isClicked: false },
+        { letter: "V", isRight: false, isClicked: false },
+        { letter: "W", isRight: false, isClicked: false },
+        { letter: "X", isRight: false, isClicked: false },
+        { letter: "Y", isRight: false, isClicked: false },
+        { letter: "Z", isRight: false, isClicked: false },
+    ]);
+    const [selectedWord, setSelectedWord] = useState("");
     const [score, setScore] = useState(300);
+
+    useEffect(() => {
+        if (theme && CategorizedWords[theme]) {
+            const randomWord =
+                CategorizedWords[theme][
+                    Math.floor(Math.random() * CategorizedWords[theme].length)
+                ];
+            setSelectedWord(randomWord.toUpperCase());
+        }
+    }, [theme]);
+
+    const handleLetterClick = (clickedLetter) => {
+        setAlphabet(
+            alphabet.map((letter) =>
+                !letter.isClicked && letter.letter === clickedLetter
+                    ? {
+                          ...letter,
+                          isRight: selectedWord.includes(clickedLetter),
+                          isClicked: true,
+                      }
+                    : letter
+            )
+        );
+    };
+
     return (
         <div className="container mx-auto p-10">
             <ProgressBar progress={10} />
             <div className="container flex mt-24">
                 <div className="w-1/2 flex items-center flex-col">
                     <ThemeCard theme={theme} />
-                    <BlankWord letter={letter} />
+                    <BlankWord letter={selectedWord} alphabet={alphabet}/>
                     <LetterWrapper>
                         {alphabet.map((letter, index) => {
-                            return letter ? <LetterCard key={index} letter={letter} /> : null;
+                            return (
+                                <LetterCard
+                                    key={index}
+                                    letter={letter.letter}
+                                    onClick={() =>
+                                        handleLetterClick(letter.letter)
+                                    }
+                                    isClicked={letter.isClicked}
+                                    isRight={letter.isRight}
+                                />
+                            );
                         })}
                     </LetterWrapper>
                 </div>
