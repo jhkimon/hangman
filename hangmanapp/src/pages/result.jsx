1956aa444a21551ca36233c07ab4bc55d84563f0
[3sua3] <[suajks0219@naver.com]>
2024-06-25 16:19:56 +0900

    sua

---
+++ hangmanapp/src/pages/result.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
+
+function Result({score, useScore}) {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        console.log('얍');
+        navigate('/');
+    };
+    return (
+        <div className="h-full w-full flex flex-col justify-center items-center">
+            <h1 className="font-grandstander font-black tracking-tighter text-center text-6xl mt-10">Congratulation</h1>
+            <p className="font-grandstander font-black tracking-tighter text-center text-4xl mt-10">score: {score}</p>
+            <br />
+            <Button onClick={handleButtonClick} letter="restart" overlayClassname=""></Button>
+        </div>
+    );
+}
+
+export default Result;
